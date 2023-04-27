8194d4abf57c6682f08859e10d154a42a1e20bdc
Luke Berry <lukeberrycs@gmail.com>
2023-04-26 22:42:21 -0700

    Branching finished for login options
    Branching completed for following test cases that begin at the login screen:
    -User clicks "log in", username is in database, and password is correct --> login
    -User clicks "log in", username in database, password does not match
    -User clicks "log in", user not in database
    -User clicks "New user", password entered is greater than 4 chars long --> user is created
    -User clicks "New user", password entered is less than 4 chars long
    -User clicks "New user", user has already been created with same username

--- app/src/main/java/com/example/fishlog/MainActivity.java
+++ app/src/main/java/com/example/fishlog/MainActivity.java
@@ -69,36 +69,59 @@ public class MainActivity extends AppCompatActivity {
     private void attemptLogin() {
         String myUsername = usernameEntry.getText().toString(); //gets values in username and password fields
         String myPassword = passwordEntry.getText().toString();
-
         User myUser = myUserDAO.findUser(myUsername); //returns the user object with the entered username
-        if(myUser.getPassword().equals(myPassword)) { //Runs if password matches username
-            setContentView(R.layout.logged_in); //sets screen to "logged in" screen
-            loginInfoBinding = LoggedInBinding.inflate(getLayoutInflater()); //creates screen from "logged_in.xml"
-            setContentView(loginInfoBinding.getRoot()); //makes layout visible
 
-            loggedInText = loginInfoBinding.loginInfoTemp; //connects textView object
-            String tempText = "You logged in with the following credentials\nUsername: " + myUsername + "\n" + "Password: " + myPassword;
-            loggedInText.setText(tempText);
-        } else { //Incorrect password
-            String incorrectPasswordPrompt = "Incorrect password";
+        boolean confirmLogin = false;
+        try {
+            if (myUser.getPassword().equals(myPassword)) { //Runs if password matches username
+                setContentView(R.layout.logged_in); //sets screen to "logged in" screen
+                loginInfoBinding = LoggedInBinding.inflate(getLayoutInflater()); //creates screen from "logged_in.xml"
+                setContentView(loginInfoBinding.getRoot()); //makes layout visible
+
+                loggedInText = loginInfoBinding.loginInfoTemp; //connects textView object
+                String tempText = "You logged in with the following credentials\nUsername: " + myUsername + "\n" + "Password: " + myPassword;
+                loggedInText.setText(tempText);
+                //LOGGING
+                System.out.println("User " + myUsername + "logged in");
+            } else { //Incorrect password
+                String incorrectPasswordPrompt = "Incorrect password";
+                errorPrompt.setText(incorrectPasswordPrompt);
+                System.out.println("Incorrect password for user " + myUsername + " entered");
+            }
+        } catch (NullPointerException e) { //Accounts for the attempt of a login for a username that has not been created
+            String incorrectPasswordPrompt = "No user found";
             errorPrompt.setText(incorrectPasswordPrompt);
+            System.out.println("User attempted a login with a username " + myUsername + " that did not exist");
         }
     }
+
 
     private void createNewUser() {
         String myUsername = usernameEntry.getText().toString(); //gets values in username and password fields
         String myPassword = passwordEntry.getText().toString();
 
-        User newUser = new User(myUsername, myPassword); //Creates a new user object with fields in username and password screen
-        myUserDAO.insert(newUser); //adds new user to the database
+        User myUser = myUserDAO.findUser(myUsername);
+        if(myUser == null) { //Runs if user with that username has NOT been created
+            if(myPassword.length() >= 4) {
+                User newUser = new User(myUsername, myPassword); //Creates a new user object with fields in username and password screen
+                myUserDAO.insert(newUser); //adds new user to the database
 
-        setContentView(R.layout.new_user); //sets screen to "new user created" screen
-        newUserBinding = NewUserBinding.inflate(getLayoutInflater()); //creates screen from "new_user.xml"
-        setContentView(newUserBinding.getRoot()); //makes layout visible
+                setContentView(R.layout.new_user); //sets screen to "new user created" screen
+                newUserBinding = NewUserBinding.inflate(getLayoutInflater()); //creates screen from "new_user.xml"
+                setContentView(newUserBinding.getRoot()); //makes layout visible
 
-        newUserText = newUserBinding.newUserTemp; //connects textView object
-        String tempText2 = "You created a new user with the following credentials\nUsername: " + myUsername + "\n" + "Password: " + myPassword;
-        newUserText.setText(tempText2);
-
+                newUserText = newUserBinding.newUserTemp; //connects textView object
+                String tempText2 = "You created a new user with the following credentials\nUsername: " + myUsername + "\n" + "Password: " + myPassword;
+                newUserText.setText(tempText2);
+                //LOGGING
+                System.out.println("New user created: " + myUsername);
+            } else {
+                errorPrompt.setText("4 char min. password");
+                System.out.println("User attempted create user " + myUsername + " with a password less than 4 characters");
+            }
+        } else {
+            errorPrompt.setText("User already created");
+            System.out.println("User attempted to create a user " + myUsername + " that had already been created");
+        }
     }
 }
